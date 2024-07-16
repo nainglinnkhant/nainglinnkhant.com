785076a9c35da2113911ce60038d7e96370284d5
nainglinnkhant <nainglinnkhnt@gmail.com>
2024-07-17 03:04:34 +0700

    chore: add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { cn } from "@/lib/utils"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
@@ -70,6 +71,7 @@ export default function RootLayout({
       <body className={cn(inter.className, "antialiased")}>
         {children}
         <Analytics />
+        <SpeedInsights />
         <TailwindIndicator />
       </body>
     </html>
