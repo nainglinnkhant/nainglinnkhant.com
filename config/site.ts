d4e87c5663d75c298c46e09ced505a886a049438
nainglinnkhant <nainglinnkhnt@gmail.com>
2024-08-15 15:15:56 +0700

    chore: update site description

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   title: "Naing Linn Khant",
   description:
-    "Software engineer who loves crafting beautiful and accessible websites",
+    "A software engineer who loves crafting beautiful and accessible websites",
   url:
     process.env.NODE_ENV === "production"
       ? "https://nainglinnkhant.com"
